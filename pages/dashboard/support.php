1cbd8481748bf4f7d92748d5495cb30af46f2450
IvanSimic95 <email@isimic.com>
2022-10-21 07:59:21 +0200

    a

--- pages/dashboard/support.php
+++ pages/dashboard/support.php
@@ -9,58 +9,6 @@ $pageTitle1 = "Support Chat";
 $sdescription = "Need help? This is the best place for you!";
 $firstTime = "";
 
-if(isset($_GET['startChat'])){
-
-//Now create new conversation
-$ch2 = curl_init();
-$data2 = [
-"participants" => ["PAadmin", "PA".$userID]
-];
-$data22 = json_encode($data2);
-curl_setopt($ch2, CURLOPT_URL, 'https://api.talkjs.com/v1/t2X08S4H/conversations/PA'.$userID);
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'PUT');
-
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $data22);
-
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-$headers[] = 'Authorization: Bearer sk_test_dmh9xKYFEPiN2BxC0Z9GuAlrdEe6kRKL';
-curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);
-
-$result2 = curl_exec($ch2);
-if (curl_errno($ch2)) {
-    echo 'Error:' . curl_error($ch2);
-}
-curl_close($ch2);
-$firstTime = "0";
-
-}else{
-
-//Check if user has chat already
-$ch2 = curl_init();
-$data2 = [];
-curl_setopt($ch2, CURLOPT_URL, 'https://api.talkjs.com/v1/t2X08S4H/users/PA'.$userID);
-curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch2, CURLOPT_CUSTOMREQUEST, 'GET');
-
-curl_setopt($ch2, CURLOPT_POSTFIELDS, $data2);
-
-$headers = array();
-$headers[] = 'Content-Type: application/json';
-$headers[] = 'Authorization: Bearer sk_test_dmh9xKYFEPiN2BxC0Z9GuAlrdEe6kRKL';
-curl_setopt($ch2, CURLOPT_HTTPHEADER, $headers);
-
-$result2 = curl_exec($ch2);
-if (curl_errno($ch2)) {
-    echo 'Error:' . curl_error($ch2);
-}
-curl_close($ch2);
-if($result2=='{"errorCode":"USER_NOT_FOUND"}'){
-$firstTime = 1;
-}
-
-}
 
 
 
@@ -135,120 +83,50 @@ $firstTime = 1;
               <div class="col-12 col-sm-12 col-xl-8 py-2 order-1 order-md-2">
                   <div class="p-0 flex-grow-1 d-flex flex-column">
 
-                  <?php if($firstTime == 1){ ?>
                   <div class="card mb-3 p-0">
                         <div class="card-header bg-light p-4 py-3 topbar-gradient">
                             <div class="d-flex flex-between-center">
-                                <h3 class="mb-0 fw-semibold fs-1" style="color:#fff;">Support Chat - Not Started Yet</h3>
+                                <h3 class="mb-0 fw-semibold fs-1" style="color:#fff;">Support - Contact Form</h3>
                             </div>
                         </div>
                         <div class="card-body px-1 px-md-2 px-lg-3 py-2">
                        
                         <div class="alert alert-info border-2 d-flex align-items-center mt-1" role="alert">
                         <div class="bg-info me-3 icon-item d-none d-sm-flex"><span class="fas fa-info-circle text-white fs-3"></span></div>
-                        <p class="mb-0 flex-1">If you need help and wish to start the chat just click the button below!</p>
+                        <p class="mb-0 flex-1">If you need help fill out this form and we will reply ASAP</p>
+                        </div>
+                        <form id="normalproduct" class="form-order needs-validation display-block" name="order_form" action="/order/order" method="get">
+
+                        <div class="form-floating">
+                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                            <option value="" disabled="" selected="" hidden="">Please choose one option</option>
+                            <option value="1">General Question</option>
+                            <option value="2">Payment</option>
+                            <option value="3">Order</option>
+                            <option value="4">User Profile</option>
+                            <option value="4">Feedback</option>
+                        </select>
+                        <label for="floatingSelect">What can we help you with? *</label>
                         </div>
 
-
-<p class="card-text placeholder-glow">
-<span class="d-block placeholder mb-2 col-10"></span>
-<span class="d-block placeholder mb-2 col-9"></span>
-<span class="d-block placeholder mb-2 col-12"></span>
-<span class="d-block placeholder mb-2 col-11"></span>
-<span class="d-block placeholder mb-2 col-9"></span>
-<span class="d-block placeholder mb-2 col-8"></span>
-<span class="d-block placeholder mb-2 col-9"></span>
-<span class="d-block placeholder mb-2 col-8"></span>
-<span class="d-block placeholder mb-2 col-12"></span>
-<span class="d-block placeholder mb-2 col-8"></span>
-<span class="d-block placeholder mb-2 col-9"></span>
-<span class="d-block placeholder mb-2 col-10"></span>
-<span class="d-block placeholder mb-2 col-9"></span>
-<span class="d-block placeholder mb-2 col-11"></span>
-<span class="d-block placeholder mb-2 col-12"></span>
-<span class="d-block placeholder mb-2 col-8"></span>
-</p>
-                        
+                        <div class="form-floating mb-3">
+                        <input class="form-control" id="emailInput" type="email" placeholder="name@example.com" name="email" value="example@gmail.com" />
+                        <label for="floatingInputValid">Your Email Address</label>
+                        </div>
                         
 
-                        <a href="?startChat=Yes" class="btn btn-dark text-uppercase w-100 mt-2">Start Chat!</a>
+                        <div class="form-floating">
+                        <textarea class="form-control" id="floatingTextarea2" placeholder="Description here" style="height: 100px"></textarea>
+                        <label for="floatingTextarea2">Description of the issue</label>
+                        </div>
+
+                        <button id="PlaceOrder" type="submit" name="form_submit" class="btn btn-submit-form btn-primary btn-shadow w-100 btn-add-to-cart mb-1 mt-1 fw-bold fs-2">Submit support request!</button></div>
+
+                            </form>
+
+                        
                         </div>
                     </div>
-
-                        <?php }else{ ?>
-
-<!-- container element in which TalkJS will display a chat UI -->
-<div id="talkjs-container" style="width: 100%; margin: 0px; height: 600px">
-  <i>Loading chat...</i>
-</div>
-
-                            <?php
-                        $customJS .= <<<EOT
-                        <script>
-                        Talk.ready.then(function() {
-                            var me = new Talk.User({
-                                id: 'PA$userID',
-                                role: 'PAcustomer',
-                                name: '$userFName',
-                                email: '$userEmail',
-                                photoUrl: 'https://avatars.dicebear.com/api/adventurer/$userEmail.svg',
-                                custom: { email: '$userEmail' }
-                            });
-                              
-                            window.talkSession = new Talk.Session({
-                            appId: 't2X08S4H',
-                            me: me,
-                            signature: "$signature"
-                            });
-                            
-                            var operator = new Talk.User({
-                                id: 'PAadmin',
-                                role: 'PAadmin',
-                                name: 'Psychic Empress',
-                                email: 'contact@psychic-empress.com',
-                                photoUrl: 'https://$domain/assets/img/logo-1.png',
-                                welcomeMessage: 'Hey, how can I help you?'
-                            });
-                            
-                            var conversation = window.talkSession.getOrCreateConversation("PA$userID");
-                            conversation.setParticipant(me);
-                            conversation.setParticipant(operator);
-                        
-                            var chatbox = window.talkSession.createChatbox(conversation);
-                            chatbox.mount(document.getElementById("talkjs-container"));
-                        
-                        
-                            
-                        window.talkSession.unreads.on('change', function (unreadConversations) {
-                          var amountOfUnreads = unreadConversations.length;
-                          var oldtitle = document.title;
-                          amountOfUnreads += document.title;
-                        
-                            $('#notifier-badge')
-                              .text(amountOfUnreads)
-                              .toggle(amountOfUnreads > 0);
-                        
-                              $('#notifier-badge-popup')
-                              .text(amountOfUnreads)
-                              .toggle(amountOfUnreads > 0);
-                          
-                            if (amountOfUnreads > 0) {
-                              document.title = '(' + amountOfUnreads + ')';
-                              $('#chat-popup').removeClass('chat-hide');
-                            } else {
-                              document.title = oldtitle;
-                              $('#chat-popup').addClass('chat-hide');
-                            }
-                          });
-                        });
-                        </script>
-                        EOT;
-
-
-                        } ?>
-
-
-
 </div>
         </div>
     </section>
