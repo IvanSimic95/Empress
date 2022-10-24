e1237c025fdb24dd0e4db6bf736f835bf8765a7a
IvanSimic95 <email@isimic.com>
2022-10-25 00:27:00 +0200

    Update t.php

--- t.php
+++ t.php
@@ -1,6 +1,7 @@
 <?php
 $orderDate = "2022-10-24 22:04:47";
+$date = new DateTime($orderDate);
 $orderPriority = "24";
-$expectedelivery = date("F d, Y h:i:s", strtotime('+'.$orderPriority.' hours', $orderDate));
+$expectedelivery = date("F d, Y h:i:s", strtotime('+'.$orderPriority.' hours', $date));
 echo $expecteddelivery;
 ?>
