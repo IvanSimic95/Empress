de34f5ae33c58c3298bfb4512252dea7e17903f1
IvanSimic95 <email@isimic.com>
2022-10-26 18:09:43 +0200

    Update logo.php

--- templates/logo.php
+++ templates/logo.php
@@ -1,2 +1,2 @@
-<img class="logo-1" src="/assets/img/logo-1.svg" alt="Psychic Empress Logo #1"> 
-     <img class="logo-2" src="/assets/img/empress.png" alt="Psychic Empress Logo #2"> 
+<img class="logo-1" src="/assets/img/logo-1.svg" alt="Psychic Empress Logo #1" style="width:50px;"> 
+     <img class="logo-2" src="/assets/img/empress-bw.png" alt="Psychic Empress Logo #2"> 
