7e88a4434a2bd361e4499af1d31ce1c849d3b500
IvanSimic95 <email@isimic.com>
2022-12-21 20:51:47 +0100

    a

--- order-processing/func/start-orders.php
+++ order-processing/func/start-orders.php
@@ -334,18 +334,17 @@ $params = array(
 $mgClient->messages()->send($domain, $params);
 
 
-
 $mg = Mailgun::create($mgkey, 'https://api.eu.mailgun.net'); // For EU servers
 
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
 $mg->messages()->send('notification.psychic-empress.com', [
   'from'    => 'noreply@notification.psychic-empress.com',
-  'to'      => 'email@isimic.com',
-  'subject' => 'The PHP SDK is awesome!',
-  'text'    => 'It is so simple to send a message.',
-  'template'=> 'template.test',
-  't:variables' => '{"EmailTitle": "Test Email Title", "orderNumber": "112233", "emailText": "Your Order is now Complete!"}'
+  'to'      => $orderEmail,
+  'subject' => 'Payment Confirmed!',
+  'text'    => 'Your Order is now Processing!',
+  'template'=> 'neworder',
+  'h:X-Mailgun-Variables' => '{"EmailTitle": "Payment Confirmed!", "orderNumber": "'.$orderID.'", "emailText": "'.$message.'", "emailButton": "'.$emailLink.'"}'
 ]);
 
 
--- test.php
+++ test.php
@@ -3,16 +3,5 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/templates/config.php';
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 use Mailgun\Mailgun;
 
-$mg = Mailgun::create($mgkey, 'https://api.eu.mailgun.net'); // For EU servers
 
-// Now, compose and send your message.
-// $mg->messages()->send($domain, $params);
-$mg->messages()->send('notification.psychic-empress.com', [
-  'from'    => 'noreply@notification.psychic-empress.com',
-  'to'      => 'email@isimic.com',
-  'subject' => 'Test Email Title',
-  'text'    => 'Your Order is now Complete!',
-  'template'=> 'neworder',
-  'h:X-Mailgun-Variables' => '{"EmailTitle": "Test Email Title", "orderNumber": "112233", "emailText": "Your Order is now Complete!"}'
-]);
 ?>
